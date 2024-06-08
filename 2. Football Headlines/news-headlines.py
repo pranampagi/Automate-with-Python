2d5ea9c9e291d72686a9d080eca020ce19daac38
Pranam Pagi <pranampagi@gmail.com>
2024-06-08 13:34:33 +0530

    Automate The News - Finding Elements

--- 2. Football Headlines/news-headlines.py
+++ 2. Football Headlines/news-headlines.py
@@ -7,3 +7,11 @@ path = "/home/pranam/Programs/Python/Automate with Python/chromedriver-linux64/c
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
 driver.get(website)
+
+containers = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
+
+
+for container in containers:
+    title = container.find_element(by="xpath", value="./a/span").text
+    subtitle = container.find_element(by="xpath", value="./a/h3").text
+    link = container.find_element(by="xpath", value="./a").get_attribute("href")
