ff82e45c8b6b3c1e8f57a18a8699dddf736dda0c
Pranam Pagi <pranampagi@gmail.com>
2024-06-08 17:42:28 +0530

    Automate The News - Preparing Script to Be Run Everyday

--- 2. Football Headlines/news-headlines.py
+++ 2. Football Headlines/news-headlines.py
@@ -2,6 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
+from datetime import datetime
+import os
+import sys
+
+application_path = os.path.dirname(sys.executable)
+
+now = datetime.now()
+month_day_year = now.strftime("%m%d%Y") # MMDDYYYY
 
 website = "https://www.thesun.co.uk/sport/football/"
 path = "/home/pranam/Programs/Python/Automate with Python/chromedriver-linux64/chromedriver"
@@ -39,6 +47,8 @@ my_dict = {
 }
 
 df_headlines = pd.DataFrame(my_dict)
-df_headlines.to_csv("headline-headless.csv")
+file_name = f"headline-{month_day_year}.csv"
+final_path = os.path.join(application_path, file_name)
+df_headlines.to_csv(final_path)
 
 driver.quit()
